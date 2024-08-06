b5336f5fb3995c7802f80de1bbfbe0f015a060eb
六 <1183417329@qq.com>
2024-08-06 10:37:13 +0800

    添加 小蚕霸王餐 的规则

---
+++ src/apps/com.realtech.xiaocan.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.realtech.xiaocan',
+  name: '小蚕霸王餐',
+  groups: [
+    {
+      key: 1,
+      name: '权限提示',
+      desc: '是否允许跳转微信，仅限本次，防止点击"始终允许"',
+      rules: [
+        {
+          activityIds: 'com.jason.videocat.ui.activity.WelcomeActivity',
+          matches: '[desc="仅限本次"]',
+        },
+      ],
+    },
+  ],
+});
